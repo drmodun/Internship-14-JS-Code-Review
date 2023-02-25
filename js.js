6d34d5b0b1da19736a6878adbbcd9b5aadd4acaf
drmodun <jan.modun.st@gmail.com>
2023-02-26 00:49:00 +0100

    made a function to make commens from data

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+const code = document.querySelector(".code-content")
 const lines = [...document.querySelectorAll(".line")];
 const comments = [...document.querySelectorAll(".comment")];
 const commentsByLine = [];
@@ -12,7 +13,37 @@ document.querySelectorAll("button").forEach(button=>{
         button.style.cursor = "default"
     })
 })
-let local = window.localStorage
+
+function MakeNewComment(text, liked, date){
+    let comment = document.createElement("div");
+    comment.classList.add("comment");
+    let commentHeader = document.createElement("div");
+    commentHeader.classList.add("comment-header");
+    const commentDate = document.createElement("div");
+    commentDate.classList.add("date");
+    let dateText = document.createTextNode(date.toLocaleString());
+    commentDate.appendChild(dateText);
+    const commentLiked = document.createElement("a");
+    const commentPicture = document.createElement("img")
+    commentPicture.src = liked ? "assets/likeon.png" : "assets/likeoff.png";
+    commentLiked.classList.add("like-button");
+    commentLiked.appendChild(commentPicture);
+    commentHeader.appendChild(commentDate);
+    commentHeader.appendChild(commentLiked);
+    comment.appendChild(commentHeader);
+    const commentContent = document.createElement("div");
+    const commentText = document.createTextNode(text);
+    commentContent.appendChild(commentText)
+    commentContent.classList.add("comment-content");
+    const deleteButton = document.createElement("button")
+    deleteButton.innerHTML = "Delete comment"
+    deleteButton.classList.add("button-delete");
+    commentContent.appendChild(deleteButton);
+    comment.appendChild(commentContent);
+    code.appendChild(comment);    
+
+}
+
 lines.forEach(element => {
     element.tabIndex = -1;
     element.parentElement.tabIndex = 1;
@@ -22,7 +53,7 @@ lines.forEach(element => {
     const lineComponents = [...element.children];
     element.addEventListener("mouseover", (event) => {
         if (isFocused)
-            return;
+        return;
         element.style.backgroundColor = "	#404040"
         element.style.cursor = "pointer"
         lineComponents.forEach(component => {
@@ -31,7 +62,7 @@ lines.forEach(element => {
     })
     element.addEventListener("mouseout", (event) => {
         if (isFocused)
-            return;
+        return;
         element.style.backgroundColor = "#28282B"
         element.style.cursor = "default"
         lineComponents.forEach(component => {
@@ -46,7 +77,7 @@ lines.forEach(element => {
         })
         element.parentElement.children[1].style.display = "flex";
     })
-
+    
     element.addEventListener("focusout", event => {
         isFocused = false
         element.style.backgroundColor = "#28282B"
@@ -58,9 +89,36 @@ lines.forEach(element => {
         element.parentElement.children[1].style.display = "none";
         
     })
+    element.addEventListener("reload", event=>{
+        const reloadComments = element.children[2];
+
+    })
 })
+let local = window.localStorage;
+if (local.getItem("comments") === null)
+    local.setItem("comments", JSON.stringify(commentsByLine))
+let localComments = JSON.parse(local.getItem("comments"));
 allCancelButtons.forEach(button=>{
     button.addEventListener("click", event=>{
         lines[allCancelButtons.indexOf(button)].dispatchEvent(closedEvent);
     });
+})
+allLocalSubmitButtons.forEach(button=>{
+    button.addEventListener("click", event=>{
+        button.parentElement.parentElement.children[2].style.display = "none"
+        const inputValue = button.parentElement.parentElement.children[0].value.trim()
+        if (inputValue.length>0){
+            const newComment = {
+                date : new Date(),
+                content : inputValue,
+                liked: false
+            };
+            console.log(newComment);
+            localComments[allLocalSubmitButtons.indexOf(button)].push(newComment);
+            local.setItem("comments", localComments)
+        }
+        else{
+            button.parentElement.parentElement.children[2].style.display = "flex"
+        }
+    })
 })
