a829f44de392b198e6b6a921e8d39ee847a7e323
drmodun <jan.modun.st@gmail.com>
2023-02-27 14:15:54 +0100

    made function to load code

--- js.js
+++ js.js
@@ -1,22 +1,138 @@
-const code = document.querySelector(".code-content")
-const lines = [...document.querySelectorAll(".line")];
-let comments = [...document.querySelectorAll(".comment")];
-const commentsByLine = [];
-let localComments = [];
-const closedEvent = new Event("close")
-const allCancelButtons = [...document.querySelectorAll(".button-cancel")];
-const allLocalSubmitButtons = [...document.querySelectorAll(".button-local")]
-let allDeleteButtons = document.querySelectorAll(".button-delete")
+function StartUp() {
+    const code = document.querySelector(".code-content")
+    const lines = [...document.querySelectorAll(".line")];
+    let comments = [...document.querySelectorAll(".comment")];
+    const commentsByLine = [];
+    let localComments = [];
+    const closedEvent = new Event("close")
+    const allCancelButtons = [...document.querySelectorAll(".button-cancel")];
+    const allLocalSubmitButtons = [...document.querySelectorAll(".button-local")]
+    let allDeleteButtons = document.querySelectorAll(".button-delete")
+    lines.forEach(element => {
+        element.tabIndex = -1;
+        const index = lines.indexOf(element);
+        element.parentElement.tabIndex = 1;
+        let isFocused = false;
+        const lineComments = [...element.children[2].querySelectorAll(".comment")];
+        commentsByLine.push(lineComments)
+        const lineComponents = [...element.children];
+        const reloadComments = element.children[2];
+        localComments.forEach(comment => {
+            if (comment.line === index) {
+                let addComment = MakeNewComment(comment.content, comment.liked, comment.date);
+                reloadComments.appendChild(addComment);
+            }
+        })
+        allDeleteButtons = [...document.querySelectorAll(".button-delete")]
+        comments = [...document.querySelectorAll(".comment")];
+        element.addEventListener("mouseover", (event) => {
+            if (isFocused)
+                return;
+            element.style.backgroundColor = "	#404040"
+            element.style.cursor = "pointer"
+            lineComponents.forEach(component => {
+                component.style.backgroundColor = "	#404040"
+            })
+        })
+        element.addEventListener("mouseout", (event) => {
+            if (isFocused)
+                return;
+            element.style.backgroundColor = "#28282B"
+            element.style.cursor = "default"
+            lineComponents.forEach(component => {
+                component.style.backgroundColor = "#28282B"
+            })
+        })
+        element.addEventListener("focus", event => {
+            isFocused = true
+            element.style.backgroundColor = "#54524c"
+            lineComponents.forEach(component => {
+                component.style.backgroundColor = "	#54524c"
+            })
+            element.parentElement.children[1].style.display = "flex";
+        })
+
+        element.addEventListener("focusout", event => {
+            isFocused = false
+            element.style.backgroundColor = "#28282B"
+            lineComponents.forEach(component => {
+                component.style.backgroundColor = "	#28282B"
+            })
+        })
+        element.addEventListener("close", event => {
+            element.parentElement.children[1].style.display = "none";
+
+        })
+    })
+    allLocalSubmitButtons.forEach(button => {
+        button.addEventListener("click", event => {
+            button.parentElement.parentElement.children[2].style.display = "none"
+            const inputValue = button.parentElement.parentElement.children[0].value.trim()
+            if (inputValue.length > 0) {
+                const newComment = {
+                    line: allLocalSubmitButtons.indexOf(button),
+                    date: new Date(),
+                    content: inputValue,
+                    liked: false
+                };
+                console.log(newComment);
+                localComments.push(newComment);
+                localComments.sort((a, b) => {
+                    if (a.line > b.line)
+                        return 1
+                    else if (a.line === b.line)
+                        return 0;
+                    else
+                        return -1;
+                });
+                local.setItem("comments", JSON.stringify(localComments));
+                console.log()
+                location.reload()
+            }
+            else {
+                button.parentElement.parentElement.children[2].style.display = "flex";
+            }
+        })
+    })
+    allDeleteButtons.forEach(button => {
+        button.addEventListener("click", event => {
+            const index = comments.indexOf(button.parentElement.parentElement)
+            comments.splice(index, 1);
+            localComments.splice(index, 1);
+            local.setItem("comments", JSON.stringify(localComments));
+            button.parentElement.parentElement.remove();
+        })
+    })
+    let allLikeButtons = [...document.querySelectorAll(".like-button")];
+    allLikeButtons.forEach(button => {
+        const index = comments.indexOf(button.parentElement.parentElement)
+        button.addEventListener("click", event => {
+            if (localComments[index].liked) {
+                button.children[0].src = "assets/likeoff.png";
+                localComments[index].liked = false
+            }
+            else {
+                button.children[0].src = "assets/likeon.png";
+                localComments[index].liked = true
+            }
+            local.setItem("comments", JSON.stringify(localComments));
+        })
+
+
+    })
+    document.querySelectorAll("button").forEach(button => {
+        button.addEventListener("mouseover", event => {
+            button.style.cursor = "pointer"
+        })
+        button.addEventListener("mouseout", event => {
+            button.style.cursor = "default"
+        })
+    })
+}
+
 const key = "drmodun";
 const baseURL = "https://homework-server1.onrender.com/"
-document.querySelectorAll("button").forEach(button => {
-    button.addEventListener("mouseover", event => {
-        button.style.cursor = "pointer"
-    })
-    button.addEventListener("mouseout", event => {
-        button.style.cursor = "default"
-    })
-})
+
 
 function MakeNewComment(text, liked, date) {
     date = new Date(date)
@@ -50,7 +166,7 @@ function MakeNewComment(text, liked, date) {
 
 }
 
-let local = window.localStorage;
+/*let local = window.localStorage;
 if (local.getItem("comments") === null)
     local.setItem("comments", JSON.stringify(localComments))
 localComments = JSON.parse(local.getItem("comments"));
@@ -59,118 +175,7 @@ allCancelButtons.forEach(button => {
         lines[allCancelButtons.indexOf(button)].dispatchEvent(closedEvent);
     });
 })
-lines.forEach(element => {
-    element.tabIndex = -1;
-    const index = lines.indexOf(element);
-    element.parentElement.tabIndex = 1;
-    let isFocused = false;
-    const lineComments = [...element.children[2].querySelectorAll(".comment")];
-    commentsByLine.push(lineComments)
-    const lineComponents = [...element.children];
-    const reloadComments = element.children[2];
-    localComments.forEach(comment => {
-        if (comment.line === index) {
-            let addComment = MakeNewComment(comment.content, comment.liked, comment.date);
-            reloadComments.appendChild(addComment);
-        }
-    })
-    allDeleteButtons = [...document.querySelectorAll(".button-delete")]
-    comments = [...document.querySelectorAll(".comment")];
-    element.addEventListener("mouseover", (event) => {
-        if (isFocused)
-            return;
-        element.style.backgroundColor = "	#404040"
-        element.style.cursor = "pointer"
-        lineComponents.forEach(component => {
-            component.style.backgroundColor = "	#404040"
-        })
-    })
-    element.addEventListener("mouseout", (event) => {
-        if (isFocused)
-            return;
-        element.style.backgroundColor = "#28282B"
-        element.style.cursor = "default"
-        lineComponents.forEach(component => {
-            component.style.backgroundColor = "#28282B"
-        })
-    })
-    element.addEventListener("focus", event => {
-        isFocused = true
-        element.style.backgroundColor = "#54524c"
-        lineComponents.forEach(component => {
-            component.style.backgroundColor = "	#54524c"
-        })
-        element.parentElement.children[1].style.display = "flex";
-    })
-
-    element.addEventListener("focusout", event => {
-        isFocused = false
-        element.style.backgroundColor = "#28282B"
-        lineComponents.forEach(component => {
-            component.style.backgroundColor = "	#28282B"
-        })
-    })
-    element.addEventListener("close", event => {
-        element.parentElement.children[1].style.display = "none";
-
-    })
-})
-allLocalSubmitButtons.forEach(button => {
-    button.addEventListener("click", event => {
-        button.parentElement.parentElement.children[2].style.display = "none"
-        const inputValue = button.parentElement.parentElement.children[0].value.trim()
-        if (inputValue.length > 0) {
-            const newComment = {
-                line: allLocalSubmitButtons.indexOf(button),
-                date: new Date(),
-                content: inputValue,
-                liked: false
-            };
-            console.log(newComment);
-            localComments.push(newComment);
-            localComments.sort((a, b) => {
-                if (a.line > b.line)
-                    return 1
-                else if (a.line === b.line)
-                    return 0;
-                else
-                    return -1;
-            });
-            local.setItem("comments", JSON.stringify(localComments));
-            console.log()
-            location.reload()
-        }
-        else {
-            button.parentElement.parentElement.children[2].style.display = "flex";
-        }
-    })
-})
-allDeleteButtons.forEach(button => {
-    button.addEventListener("click", event => {
-        const index = comments.indexOf(button.parentElement.parentElement)
-        comments.splice(index, 1);
-        localComments.splice(index, 1);
-        local.setItem("comments", JSON.stringify(localComments));
-        button.parentElement.parentElement.remove();
-    })
-})
-let allLikeButtons = [...document.querySelectorAll(".like-button")];
-allLikeButtons.forEach(button => {
-    const index = comments.indexOf(button.parentElement.parentElement)
-    button.addEventListener("click", event => {
-        if (localComments[index].liked) {
-            button.children[0].src = "assets/likeoff.png";
-            localComments[index].liked = false
-        }
-        else {
-            button.children[0].src = "assets/likeon.png";
-            localComments[index].liked = true
-        }
-        local.setItem("comments", JSON.stringify(localComments));
-    })
-
-
-})
+*/
 /*function GetComments() {
     fetch(baseURL + "comments", {
         headers: {
@@ -192,19 +197,19 @@ async function GetCode() {
         })
         if (!response.ok)
             throw response.status;
-        else{
-            const returnValue = await response.json(); 
+        else {
+            const returnValue = await response.json();
             console.log(returnValue)
             const code = returnValue.code.split("\n");
             console.log(code)
             return code;
         }
     }
-    catch(err){
-        console.error(err); 
+    catch (err) {
+        console.error(err);
     }
 }
-async function GetComments(){
+async function GetComments() {
     try {
         const response = await fetch(baseURL + "comments", {
             headers: {
@@ -214,16 +219,88 @@ async function GetComments(){
         })
         if (!response.ok)
             throw response.status;
-        else{
-            const returnValue = await response.json(); 
+        else {
+            const returnValue = await response.json();
             console.log(returnValue)
             return returnValue;
         }
     }
-    catch(err){
-        console.error(err); 
+    catch (err) {
+        console.error(err);
+        return ""
     }
 }
-function MakeNewLine(){
-    return null;
+function MakeNewLine(text, comments, lineNumber) {
+    const lineRow = document.createElement("div");
+    lineRow.classList.add("line-main");
+    const line = document.createElement("div");
+    line.classList.add("line");
+    const lineNumberLabel = document.createElement("span");
+    lineNumberLabel.classList.add("line-number");
+    lineNumberLabel.innerHTML = lineNumber+1;
+    const lineContent = document.createElement("span");
+    lineContent.classList.add("line-content");
+    lineContent.innerHTML = text;
+    const lineComments =  document.createElement("div");
+    lineComments.classList.add("line-comments")
+    //add comments much later
+    line.appendChild(lineNumberLabel);
+    line.appendChild(lineContent);
+    line.appendChild(lineComments);
+    const newCommentRow = document.createElement("div");
+    const newComment = document.createElement("div")
+    newCommentRow.classList.add("new-comment");
+    newComment.classList.add("comment", "user-input");
+    let commentHeader = document.createElement("div");
+    commentHeader.classList.add("comment-header");
+    commentHeader.innerHTML = "This is your new comment";
+    newComment.appendChild(commentHeader);
+    const commentContent = document.createElement("div");
+    const commentTextArea = document.createElement("textarea");
+    const commentButtons = document.createElement("div");
+    commentButtons.classList.add("comment-buttons");
+    commentTextArea.rows = 10;
+    commentTextArea.cols = 20;
+    commentTextArea.wrap = "hard";
+    commentTextArea.innerHTML = "Write comment text here";
+    commentTextArea.classList.add("comment-input");
+    commentContent.appendChild(commentTextArea);
+    commentContent.classList.add("comment-content");
+    const cancelButton = document.createElement("button")
+    cancelButton.innerHTML = "Cancel"
+    cancelButton.classList.add("button-cancel");
+    const makePrivateCommentButton = document.createElement("button")
+    makePrivateCommentButton.innerHTML = "Save as private Note"
+    makePrivateCommentButton.classList.add("button-local");
+    commentButtons.appendChild(makePrivateCommentButton);
+    const makeServerCommentButton = document.createElement("button")
+    makeServerCommentButton.innerHTML = "Save to server"
+    makeServerCommentButton.classList.add("button-server");
+    commentButtons.appendChild(makeServerCommentButton);
+    commentButtons.appendChild(cancelButton);
+    commentContent.appendChild(commentButtons);
+    const errorMessage = document.createElement("span");
+    errorMessage.classList.add("error-message");
+    errorMessage.innerHTML = "You cannot send an empty comment";
+    commentContent.appendChild(errorMessage);
+    newComment.appendChild(commentContent);
+    newCommentRow.appendChild(newComment);
+    lineRow.appendChild(line);
+    lineRow.appendChild(newCommentRow);
+    const insertLocation = document.querySelector(".code-content");
+    insertLocation.appendChild(lineRow);
+}
+async function ConstructCode() {
+    try {
+        const code = await GetCode();
+        if (code.length === 0)
+            throw "Error on Code request";
+        code.forEach(line => {
+            MakeNewLine(line, {}, code.indexOf(line));
+        })
+        StartUp();
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
